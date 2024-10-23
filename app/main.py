f7903046c30ecd1d43aae689961a972417fec1fa
анастасия <hutjicsgo@gmail.com>
2024-10-23 14:44:47 +0300

    feat/logging

--- app/main.py
+++ app/main.py
@@ -7,17 +7,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from app.database import get_async_db
+from app.logger import setup_logger
 from app.models import CurrencyRate
 
 app = FastAPI()
+logger = setup_logger()
 
 redis_client = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 
 @app.get("/rates")
 async def get_rates(db: AsyncSession = Depends(get_async_db)):
+    logger.info("Достаем стоимость из БД")
     cached_rates = await redis_client.get("all_rates")
     if cached_rates:
+        logger.info("Возвращаем кешированные данные")
         return json.loads(cached_rates)
 
     result = await db.execute(select(CurrencyRate))
@@ -25,6 +29,7 @@ async def get_rates(db: AsyncSession = Depends(get_async_db)):
     await redis_client.set(
         "all_rates", json.dumps([rate.to_dict() for rate in rates])
     )
+    logger.info("Возвращаем последние данные")
     return rates
 
 
@@ -32,8 +37,10 @@ async def get_rates(db: AsyncSession = Depends(get_async_db)):
 async def get_rates_by_symbol(
     symbol: str, db: AsyncSession = Depends(get_async_db)
 ):
+    logger.info(f"Получаем стоимость по символу {symbol}")
     cached_rate = await redis_client.get(f"rate_{symbol}")
     if cached_rate:
+        logger.info(f"Возвращем кешированную стоимость по символу {symbol}")
         return json.loads(cached_rate)
 
     result = await db.execute(
@@ -41,9 +48,8 @@ async def get_rates_by_symbol(
     )
     rate = result.scalars().first()
     if rate is None:
-        raise HTTPException(
-            status_code=404, detail="Ошибка: Символ не найден."
-        )
+        logger.warning(f"Символ не найден: {symbol}")
+        raise HTTPException(status_code=404, detail="Ошибка: Символ не найден")
     await redis_client.set(f"rate_{symbol}", json.dumps(rate.to_dict()))
     return rate
 
@@ -51,9 +57,11 @@ async def get_rates_by_symbol(
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    logger.info("Установлено соединение с вебсокетом")
     try:
         while True:
             data = await websocket.receive_text()
+            logger.info(f"Получено сообщение: {data}")
             await websocket.send_text(f"Сообщение: {data}")
     except Exception as err:
         print(err)
